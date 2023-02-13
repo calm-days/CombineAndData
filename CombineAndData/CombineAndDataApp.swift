257bc5de4b11d3d4bcdd0bc035a239536b1b59f9
Roman Liukevich <r.liukevich@gmail.com>
2023-02-13 16:18:24 +0100

    Initial Commit

---
+++ CombineAndData/CombineAndDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAndDataApp.swift
+//  CombineAndData
+//
+//  Created by Roman Liukevich on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct CombineAndDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineAndData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineAndData
+//
+//  Created by Roman Liukevich on 2/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
